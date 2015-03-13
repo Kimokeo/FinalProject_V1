10e4cc732eee8fb1292aeabee5b41c9041cdf6bd
Kimokeo Rabang <kimokerava@gmail.com>
2015-03-13 16:10:05 -0400

    first commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,25 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require twitter/bootstrap
+//= require turbolinks
+//= require_tree .
+// Vendor Files
+// = require MIDI/AudioDetect
+// = require MIDI/LoadPlugin
+// = require MIDI/Plugin
+// = require MIDI/Player
+// = require Window/DOMLoader.XMLHttp
+// = require inc/Base64
+// = require inc/base64binary
